24b489e14b2700f386fdaea580a93c3613602e14
Aurino Salvador <aurinosalvador@gmail.com>
2021-09-03 14:26:00 -0300

    Convert CardList to stateless.

--- lib/widgets/card_list.dart
+++ lib/widgets/card_list.dart
@@ -3,31 +3,26 @@ import 'package:escribo_teste_03/models/movie.dart';
 import 'package:escribo_teste_03/models/people.dart';
 import 'package:flutter/material.dart';
 
-class CardList<T> extends StatefulWidget {
+class CardList<T> extends StatelessWidget {
   final T object;
   final Function? onTap;
 
   const CardList(this.object, {Key? key, this.onTap}) : super(key: key);
 
   @override
-  State<CardList<T>> createState() => _CardListState<T>();
-}
-
-class _CardListState<T> extends State<CardList<T>> {
-  @override
   Widget build(BuildContext context) {
     Color borderColor = Colors.black;
     String text;
     bool isFav = false;
-    if (widget.object.runtimeType == Movie) {
-      text = (widget.object as Movie).title;
-      isFav = (widget.object as Movie).isFavorite();
-    } else if (widget.object.runtimeType == People) {
-      text = (widget.object as People).name;
-      isFav = (widget.object as People).isFavorite();
+    if (object.runtimeType == Movie) {
+      text = (object as Movie).title;
+      isFav = (object as Movie).isFavorite();
+    } else if (object.runtimeType == People) {
+      text = (object as People).name;
+      isFav = (object as People).isFavorite();
     } else {
-      text = (widget.object as Favorite).description;
-      if ((widget.object as Favorite).type == 'movie') {
+      text = (object as Favorite).description;
+      if ((object as Favorite).type == 'movie') {
         borderColor = Colors.red;
       } else {
         borderColor = Colors.green;
@@ -51,9 +46,9 @@ class _CardListState<T> extends State<CardList<T>> {
               textScaleFactor: 1.2,
               textAlign: TextAlign.center,
             ),
-            if (widget.object.runtimeType != Favorite)
+            if (object.runtimeType != Favorite)
               GestureDetector(
-                onTap: () => widget.onTap!(widget.object),
+                onTap: () => onTap!(object),
                 child: Icon(isFav ? Icons.favorite : Icons.favorite_border),
               ),
           ],
